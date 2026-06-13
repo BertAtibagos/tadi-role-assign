7512025fd855805f79467c7d091d1dd55629c811
BertAtibagos <atibagos.robert2300@gmail.com>
2026-06-13 12:54:01 +0800

    Added new useEffect for section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { useYears } from './hooks/useYears';
 import { usePeriod } from './hooks/usePeriod';
 import { usePrograms } from './hooks/usePrograms';
+import { useSections } from './hooks/useSections';
 import SearchFilterCard from './components/SearchFilterCard';
 
 const App = () => {
@@ -10,29 +11,44 @@ const App = () => {
   const [selectedYearLevel, setSelectedYearLevel] = useState('')
   const [selectedPrd, setSelectedPrd] = useState('')
   const [selectedPrgm, setSelectedPrgm] = useState('')
+  const [selectedSect, setSelectedSect] = useState('')
   const { programs, prgErr, fetchPrograms } = usePrograms()
+  const { sections, sectionErr, fetchSections } = useSections();
 
   useEffect(() => {
     if(selectedYearLevel && selectedPrd){
       fetchPrograms({yearId: selectedYearLevel, periodId: selectedPrd})
     }},[selectedYearLevel, selectedPrd])
 
+  useEffect(()=>{
+    if(selectedYearLevel && selectedPrd && selectedPrgm){ 
+      fetchSections(selectedYearLevel, selectedPrd, selectedPrgm)
+    }
+  },[selectedYearLevel, selectedPrd, selectedPrgm])
+
   return (
     <div className="min-h-screen bg-slate-950 text-slate-100">
       <div className="mx-auto w-full max-w-3xl px-4 py-10">
         <SearchFilterCard 
-          years={years} 
-          yrErr={yrErr} 
+          years={years}  
           period={period} 
-          prdErr={prdErr}
-          slctYr={selectedYearLevel}
-          ocSlctYr={setSelectedYearLevel}
-          slctPrd={selectedPrd}
-          ocSlctPrd={setSelectedPrd}
           programList={programs}
+          sectionList={sections}
+
+          yrErr={yrErr}
+          prdErr={prdErr}
           prgmErr={prgErr}
+          sectionErr={sectionErr}
+
+          slctYr={selectedYearLevel}
+          slctPrd={selectedPrd}
           slctPrgm={selectedPrgm}
+          slctSect={selectedSect}
+
+          ocSlctYr={setSelectedYearLevel}
+          ocSlctPrd={setSelectedPrd}
           ocSlctPrgm={setSelectedPrgm}
+          ocSlctSect={setSelectedSect}
         />
       </div>
     </div>
