7b813a7a16802bee8e00f024aa72ade0ca918f6b
BertAtibagos <atibagos.robert2300@gmail.com>
2026-06-17 16:20:54 +0800

    changed the majority of custom hook

--- src/hooks/useClassList.js
+++ src/hooks/useClassList.js
@@ -1,39 +1,32 @@
-import {useState, useRef } from 'react';
+import { useState, useRef } from 'react';
 
-export function useSearchResult(){
+export function useClassList(){
     const [data, setData] =useState([]);
-    const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
     const cache = useRef(null);
 
-    setIsLoading(true);
-
-    const searchRes = async ({year, period}={}) => {
+    const fetchSearchRes = async (year, period, section) => {
         if (cache.current) cache.current.abort();
         cache.current = new AbortController();
         const signal = cache.current.signal;
 
         try{
-            const params = new URLSearchParams();
-            if(year) params.append('year', year);
-            if(period) params.append('period', period);
+            const searchResReq = await fetch(`api/classList`,{
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({yearId: year, periodId: period, sectionId: section}),
+                signal
+            });
 
-            const req = await fetch(`api/year-period?${params.toString()}`, { signal });
-            const res = await req.json();
+            const searchResRes = await searchResReq.json();
 
-            if(!req.ok) throw new Error(res.message || 'Failed to fetch data');
-            setData(res.data || []);
+            if(!searchResReq.ok) throw new Error(searchResRes.message || 'Failed to fetch data');
+            setData(searchResRes.data || []);
         }
         catch(err){
             if (err.name === 'AbortError') setError(err.message);
-        }finally{
-            setIsLoading(false)
         }
     };
 
-    const cancelSearch = () => {
-        if (cache.current) cache.current.abort();
-    };
-
-    return {data, isLoading, error, searchRes, cancelSearch};
+    return {data, error, fetchSearchRes};
 }
