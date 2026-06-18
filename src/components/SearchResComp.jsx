9054f082c146947100451c16aac90e589efff86a
BertAtibagos <atibagos.robert2300@gmail.com>
2026-06-18 16:31:19 +0800

    Fixed Ui layout bug

--- src/components/SearchResComp.jsx
+++ src/components/SearchResComp.jsx
@@ -1,25 +1,29 @@
 import { useState} from 'react';
 import InputField from './InputField';
 import Button from './Button';
+import ClassListTable from './ClassListTable';
 
 export default function SearchResComp() {
     const [srchInput, setSrchInput] = useState('');
-    const data = false;
+    const data = true; 
 
     return(
-        <div className="flex flex-col gap-3 md:flex-row md:items-end justify-center">
-            { data ? <>
-                        <InputField
-                            style="w-full md:flex-1"
-                            text="Search by name"
-                            value={srchInput}
-                            onChange={(e) => setSrchInput(e.target.value)}
-                        />
-                        <Button style="w-full md:w-28 bg-blue-600/60">
-                                Search
-                        </Button>
-                    </> 
-            : <p className="text-slate-400">Select all from the filter above to start searching</p>}
-        </div>
+             data ? <>
+                        <div className="flex flex-col md:flex-row gap-3 w-full">
+                            <InputField
+                                style="w-full md:flex-1"
+                                text="Search by name"
+                                value={srchInput}
+                                onChange={(e) => setSrchInput(e.target.value)}
+                            />
+                            <Button style="w-full md:w-28 bg-blue-600/60">
+                                    Search
+                            </Button>
+                        </div>
+                        <ClassListTable />
+                    </>
+            : <div className="flex justify-center">
+                <p className="text-slate-400">Select all from the filter above to start searching</p>
+            </div>
     );
 }
