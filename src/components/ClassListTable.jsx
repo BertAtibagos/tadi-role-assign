fbff7bbda157a4f44dcfc5da3fd3ce70ab00539f
BertAtibagos <atibagos.robert2300@gmail.com>
2026-06-22 09:39:43 +0800

    Add props to map data form API

--- src/components/ClassListTable.jsx
+++ src/components/ClassListTable.jsx
@@ -1,5 +1,5 @@
-
-export default function ClassListTableComp() {
+export default function ClassListTableComp(props) {
+    const { classListData } = props;
     return(
         <div>
             <table className="w-full table-auto border-collapse text-left">
@@ -14,15 +14,19 @@ export default function ClassListTableComp() {
                     </tr> 
                 </thead>
                 <tbody>
-                    <tr className="border-b border-white/10">
-                        <td className="px-4 py-2 text-sm text-slate-100">
-                            <input type="checkbox" className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500" />
-                        </td>
-                        <td className="px-4 py-2 text-sm text-slate-100">Robert Galapin Atibagos</td>
-                        <td className="px-4 py-2 text-sm text-slate-100">
-                            <button className="rounded bg-green-600/60 px-3 py-1 text-sm text-white hover:bg-green-600/80">Assign</button>
-                        </td>
-                    </tr>
+                    {classListData.map(item =>(
+                        <tr className="border-b border-white/10" key={item.student_id}>
+                            <td className="px-4 py-2 text-sm text-slate-100">
+                                <input type="checkbox" className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500" />
+                            </td>
+                            <td className="px-4 py-2 text-sm text-slate-100">{item.student_name}</td>
+                            <td className="px-4 py-2 text-sm text-slate-100">
+                                <button className="rounded bg-green-600/60 px-3 py-1 text-sm text-white hover:bg-green-600/80">Assign</button>
+                            </td>
+                        </tr>
+                        )
+                        )
+                    }
                 </tbody>
             </table>
         </div>
