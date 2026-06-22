dc675a9a458788f86cd8279583b42c9a9e212176
BertAtibagos <atibagos.robert2300@gmail.com>
2026-06-22 10:18:04 +0800

    Changed the condtion for dyanmic display

--- src/components/SearchResComp.jsx
+++ src/components/SearchResComp.jsx
@@ -10,22 +10,22 @@ export default function SearchResComp(props) {
     if(classErr) return <p className="text-red-500">Something went wrong.</p>;
 
     return(
-             classList ? <>
-                        <div className="flex flex-col md:flex-row gap-3 w-full">
-                            <InputField
-                                style="w-full md:flex-1"
-                                text="Search by name"
-                                value={srchInput}
-                                onChange={(e) => setSrchInput(e.target.value)}
+             classList.length ? <>
+                            <div className="flex flex-col md:flex-row gap-3 w-full">
+                                <InputField
+                                    style="w-full md:flex-1"
+                                    text="Search by name"
+                                    value={srchInput}
+                                    onChange={(e) => setSrchInput(e.target.value)}
+                                />
+                                <Button style="w-full md:w-28 bg-blue-600/60">
+                                        Search
+                                </Button>
+                            </div>
+                            <ClassListTable
+                                classListData={classList}
                             />
-                            <Button style="w-full md:w-28 bg-blue-600/60">
-                                    Search
-                            </Button>
-                        </div>
-                        <ClassListTable
-                            classListData={classList}
-                        />
-                    </>
+                        </>
             : <div className="flex justify-center">
                 <p className="text-slate-400">Select all from the filter above to start searching</p>
             </div>
