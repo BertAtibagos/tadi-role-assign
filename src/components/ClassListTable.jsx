33a93c8ee515d896b5b965539d2fd584af57bf97
BertAtibagos <atibagos.robert2300@gmail.com>
2026-06-27 10:58:30 +0800

    Added temporary button and button function

--- src/components/ClassListTable.jsx
+++ src/components/ClassListTable.jsx
@@ -1,3 +1,13 @@
+import { useState } from 'react';
+
+const ActionButton = (props) => {
+    const {onClick, className, children} = props
+    return(
+            <button className={className} onClick={onClick}>
+                { children }
+            </button>)
+}
+
 export default function ClassListTableComp(props) {
     const { classListData } = props;
     return(
@@ -9,7 +19,6 @@ export default function ClassListTableComp(props) {
                             <input type="checkbox" className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500" />
                         </th> */}
                         <th className="px-4 py-2 text-sm font-medium text-slate-400">Student Name</th>
-                        <th className="px-4 py-2 text-sm font-medium text-slate-400">Status</th>
                         <th className="px-4 py-2 text-sm font-medium text-slate-400">Action</th>   
                         <th></th>
                     </tr> 
@@ -22,13 +31,11 @@ export default function ClassListTableComp(props) {
                             </td> */}
                             <td className="px-4 py-2 text-sm text-slate-100">{item.student_name}</td>
                             <td className="px-4 py-2 text-sm text-slate-100">
-                                <span className="rounded px-2 py-1 text-xs bg-green-600/20 text-green-400">
-                                    Assigned
-                                </span>
-                            </td>
-                            <td className="px-4 py-2 text-sm text-slate-100">
-                                {/* <button className="rounded bg-green-600/60 px-3 py-1 text-sm text-white hover:bg-green-600/80">Assign</button> */}
-                                <input type="checkbox" className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500" />
+                                <ActionButton 
+                                    className="rounded bg-green-600/60 px-3 py-1 text-sm text-white hover:bg-green-600/80"
+                                    onClick={() => alert(`Viewing ${item.student_name}`)}>
+                                    Assign
+                                </ActionButton>
                             </td>
                         </tr>
                         )
